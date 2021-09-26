f7c6beecd42dd3931888e0bd8f0b6876988620c8
Shai Pilosof <shainova@gmail.com>
2021-09-26 14:00:17 +0300

    Update create_multilayer_object.R
    Only allow extended edge list format in interlayer edges. Enable writing edge list to a txt file

--- R/create_multilayer_object.R
+++ R/create_multilayer_object.R
@@ -10,8 +10,8 @@
 #'   \code{inter} are ignored. Nodes and layers should be specified by IDs, not names.
 #' @param intra An edge list of the format \code{layer node_from node_to
 #'   weight}. Nodes and layers should be specified by IDs, not names.
-#' @param inter An edge list of the format \code{layer_from node_from layer_to
-#'   node_to weight}. Nodes and layers should be specified by IDs, not names. Defaults to NULL,
+#' @param inter An extended edge list of the format \code{layer_from node_from
+#'  layer_to node_to weight}. Nodes and layers should be specified by IDs, not names. Defaults to NULL,
 #'   assuming the network does not have interlayer edges.
 #' @param nodes A data frame with node metadata. First column must be node_id
 #'   with IDs corresponding to those in \code{extended}, \code{intra} or
@@ -20,22 +20,23 @@
 #'   with IDs corresponding to those in \code{extended}, \code{intra} or
 #'   \code{inter}.
 #' @param intra_output_extended Defaults to \code{TRUE}. Should the intralayer edges be written in the extended edge list format.
-#' @param inter_output_extended Defaults to \code{TRUE}. Should the intralayer edges be written in the extended edge list format.
+#' @param write_to_file Write the edgel list to a text file? Convenient to copy-paste into the online version of infomap.
+#' @param filename_prefix The name of the file. Extension will always be txt and the file is space-delimited.
+#' 
 #'
 #' @details \code{extended, intra, inter} must only contain node IDs. Any
-#'   details on nodes and layers that link to these IDs should be provided in
+#'   node and layer attributes that link to these IDs should be provided in
 #'   \code{nodes} and \code{layers}.
 #'
 #'   Intralayer edges can have either an extended format of \code{layer_from
-#'   node_from layer_to node_to weight} or a non-extended Intra/Inter format of \code{layer
+#'   node_from layer_to node_to weight} or a non-extended format of \code{layer
 #'   node_from node_to weight}.
 #'
-#'   Interlayer edges too can have either an extended format of \code{layer_from
-#'   node_from layer_to node_to weight} or a non-extended format of
-#'   \code{layer_from node layer_to weight}. **However, in Infomap, a non-extended format is
-#'   ONLY valid for coupling interlayer edges using a relax rate, which connects between different
-#'   realizations of the same physical nodes in diffrent layers. When interlayer
-#'   edges are between different physical nodes use the extended format.**
+#'   Interlayer edges must have an extended format of \code{layer_from
+#'   node_from layer_to node_to weight}. This is because a non-extended format (\code{layer_from
+#'   node_from layer_to weight}) is ONLY valid for interlayer edges that connect between different
+#'   realizations of the same physical nodes in different layers (coupling interlayer links). To avoid
+#'   confusion we do not allow this format.
 #'
 #' @examples
 #' data("siberia1982_7_links") # Links are an extended edge list
@@ -70,7 +71,7 @@
 ## @import dplyr
 ## @import magrittr
 
-create_multilayer_object <- function(extended=NULL, intra=NULL, inter=NULL, nodes=NULL, intra_output_extended=T, inter_output_extended=T, layers=NULL){
+create_multilayer_object <- function(extended=NULL, intra=NULL, inter=NULL, nodes=NULL, intra_output_extended=T, layers=NULL, write_to_file=F, filename_prefix=NULL){
   if(!is.null(layers)){
     if(names(layers)[1]!='layer_id') {stop('First column in layers must be named layer_id')}
   }
@@ -83,21 +84,22 @@ create_multilayer_object <- function(extended=NULL, intra=NULL, inter=NULL, node
     inter <- extended %>% filter(layer_from!=layer_to)
     # If there are no interlayer edges
     if (nrow(inter)==0){inter <- NULL}
-
+    
     # Set the output formats
     if (intra_output_extended==F){intra %<>% select(layer=layer_from, node_from, node_to, weight)}
-    if (!is.null(inter) && inter_output_extended==F){inter %<>% select(layer_from, node=node_from, layer_to, weight)}
-
+    # Write edge list to a txt file
+    if (write_to_file==T){write_delim(extended, paste(filename_prefix,'.txt',sep=''), delim = ' ')}
   } else {
     if (!is.null(intra)){if(names(intra)[4]!='weight') {stop('4th column in Intralayer edge list should be weight')}}
-    if (!is.null(inter)){if(names(intra)[4]!='weight') {stop('4th column in Interlayer edge list should be weight')}}
+    if (!is.null(inter)){if(names(inter)[5]!='weight') {stop('5th column in Interlayer edge list should be weight')}}
     
-    # for extended output of intralayer edges
+    # for non-extended output of intralayer edges
     if (intra_output_extended) {intra %<>% select(layer_from=layer, node_from, layer_to=layer, node_to, weight)}
-    if (!is.null(inter) && inter_output_extended==F){inter %<>% select(layer_from, node=node_from, layer_to, weight)}
+    # Write edge list to a txt file
+    write_delim(intra, paste(filename_prefix,'.txt',sep=''), delim = ' ')
+    if (!is.null(inter)){write_delim(inter, paste(filename_prefix,'.txt',sep=''), delim = ' ', append = T)}
   }
   
-   
   out <- list(intra=intra,
               inter=inter,
               nodes=nodes,
