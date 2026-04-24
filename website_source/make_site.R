9d9533ead491d1d9e99eaaddb38b7e2d1a9cbe54
Shai Pilosof <shainova@gmail.com>
2026-04-24 13:41:12 +0300

    Organize fils and folders

--- website_source/make_site.R
+++ website_source/make_site.R
@@ -3,7 +3,7 @@ library(infomapecology)
 remotes::install_github("rlesur/klippy")
 
 #setwd("/Users/shai/GitHub/ecomplab/emln_package")
-setwd("/Users/shirlyf/Documents/GitHub/infomap_ecology_package")
+# setwd("~/GitHub/infomap_ecology_package")
 
 # Make the website
 rmarkdown::render_site(input = 'website_source')
@@ -16,5 +16,6 @@ rmarkdown::render_site(input = 'website_source')
 # Remove unnecesary files
 unlink('docs/', recursive = TRUE, force = FALSE)
 file.copy('website_source/docs/', '.', recursive = T)
+unlink('docs/Infomap')
 unlink('website_source/analysis_example_files/', recursive = TRUE, force = FALSE)
 unlink('website_source/docs/', recursive = TRUE, force = FALSE)
